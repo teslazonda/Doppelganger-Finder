d3f875dc4948acf35ade0e32f217f3eeae1cb994
David Gregorato <HydroSnake@gmail.com>
2021-08-16 19:59:36 +0900

    Added the booking model and connected them to the doppelganger and the user models.

---
+++ db/migrate/20210816105436_create_bookings.rb
@@ -0,0 +1,15 @@
+class CreateBookings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :bookings do |t|
+      t.datetime :start_time
+      t.datetime :end_time
+      t.string :location
+      t.integer :status
+      t.references :doppelganger, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.text :instructions
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_08_16_091752) do
-
-
+ActiveRecord::Schema.define(version: 2021_08_16_105436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.string "location"
+    t.integer "status"
+    t.bigint "doppelganger_id", null: false
+    t.bigint "user_id", null: false
+    t.text "instructions"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["doppelganger_id"], name: "index_bookings_on_doppelganger_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "doppelgangers", force: :cascade do |t|
     t.string "name"
@@ -29,7 +39,6 @@ ActiveRecord::Schema.define(version: 2021_08_16_091752) do
     t.index ["user_id"], name: "index_doppelgangers_on_user_id"
   end
 
-
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password"
@@ -40,6 +49,7 @@ ActiveRecord::Schema.define(version: 2021_08_16_091752) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "bookings", "doppelgangers"
+  add_foreign_key "bookings", "users"
   add_foreign_key "doppelgangers", "users"
-
 end
