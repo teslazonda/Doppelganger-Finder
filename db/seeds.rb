65437357103033efd659c8cbe6124353f2d17c77
Kevin Konczak <konczakkevin@gmail.com>
2021-08-17 11:46:51 +0900

    added faker and created the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning the DB'
+Doppelganger.destroy_all
+puts 'DB is clean'
+
+puts 'Creating new Doppelgangers from Breaking Bad'
+5.times do
+  Doppelganger.create(
+    name: Faker::TvShows::BreakingBad.character,
+    description: Faker::Lorem.sentence(word_count: 10)
+  )
+end
+puts 'Breaking Bad characters created'
+
+puts 'Creating new Doppelgangers from Star Wars'
+5.times do
+  Doppelganger.create(
+    name: Faker::Movies::StarWars.character,
+    description: Faker::Movies::StarWars.quote
+  )
+end
+puts 'Star Wars characters created'
+
+puts 'Creating new Doppelgangers from Harry Potter'
+5.times do
+  Doppelganger.create(
+    name: Faker::Movies::HarryPotter.character,
+    description: Faker::Movies::HarryPotter.quote
+  )
+end
+puts 'Harry Potter characters created'
+
+puts 'Creating a very dear doppelganger to us'
+Doppelganger.create(
+  name: 'Conan O\'Brien',
+  description: 'If life gives you lemons, make some kind of fruity juice.'
+)
