f51ac5449dc7bf364ca878ece03b20991758d6aa
Kevin Konczak <konczakkevin@gmail.com>
2021-08-17 14:22:10 +0900

    fixed the migration, now the photo url is in the model

--- db/migrate/20210817051701_add_photo_url_to_doppelgangers.rb
+++ db/migrate/20210817051701_add_photo_url_to_doppelgangers.rb
@@ -1,4 +1,4 @@
-class AddImageToDoppelgangers < ActiveRecord::Migration[6.0]
+class AddPhotoUrlToDoppelgangers < ActiveRecord::Migration[6.0]
   def change
     add_column :doppelgangers, :photo_url, :string
   end
