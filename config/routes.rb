5e2ba7ac0866eee5cfb1d29f8aef69a25aa3e882
David Gregorato <HydroSnake@gmail.com>
2021-08-17 11:21:42 +0900

    Added routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :doppelganger
+  resources :doppelganger, only: %w[index new create show] do
+    resources :booking, only: %w[new create]
+  end
+  resources :booking, only: %w[update]
 end
