0b1a8e06558b9c36461cf1a0137d18e0b1d80e65
Quill Cronwall <quilljazz@gmail.com>
2021-08-17 12:50:52 +0900

    implement devise

--- db/schema.rb
+++ db/schema.rb
@@ -40,13 +40,18 @@ ActiveRecord::Schema.define(version: 2021_08_16_105436) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password"
     t.string "first_name"
     t.string "last_name"
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "bookings", "doppelgangers"
